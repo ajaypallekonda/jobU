e44080438b4cec1890f3d76bee1d22a938621e3f
ajaypallekonda <32649063+ajaypallekonda@users.noreply.github.com>
2020-10-04 05:17:54 -0700

    update

--- Python/Backend.py
+++ Python/Backend.py
@@ -1,23 +1,3 @@
-# from webScrape import WebScrape
-# from flask import Flask, redirect, url_for, request, redirect
-# import json
-
-# app = Flask(__name__)
-
-# @app.route('/internships')
-# def run():
-#     webscraper = WebScrape()
-#     webscraper.run()
-#     with open('C:/Users/ajayj/Desktop/Github/jobU/JSONWebServer/internships.json') as f:
-#         data = json.load(f)
-#     return json.dumps(data)
-
-# @app.route('/', methods = ['GET'])
-# def called():
-#     return redirect("/internships")
-
-# if __name__ == '__main__':
-#     app.run()
 from flask import Flask, redirect, url_for, request, redirect
 from flask_restful import Resource, Api
 from webScrape import WebScrape
@@ -30,9 +10,6 @@ class HelloWorld(Resource):
     def get(self):
         webscraper = WebScrape()
         webscraper.run()
-        with open('C:/Users/ajayj/Desktop/Github/jobU/JSONWebServer/internships.json') as f:
-            data = json.load(f)
-        return data
 
 api.add_resource(HelloWorld, '/')
 
